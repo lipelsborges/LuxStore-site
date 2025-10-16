c58181510edae974c4a853d5f60cb83f977f0cee
Felipe Borges <lipelsborges@gmail.com>
2025-10-16 16:25:04 -0300

    final updates

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const prevButton = document.getElementById('prev')
-const nextButton =  document.getElementById('next')
+const nextButton = document.getElementById('next')
 const items = document.querySelectorAll('.item')
 const dots = document.querySelectorAll('.dot')
 const numberIndicator = document.querySelector('.numbers')
@@ -9,21 +9,39 @@ let active = 0;
 const total = items.length
 let timer;
 
-function update(direcao){
+function update(direcao) {
 
     document.querySelector('.item.active').classList.remove('active')
     document.querySelector('.dot.active').classList.remove('active')
 
-    if(direcao > 0){
-        active = active + 1
-    } 
-    
-    else if(direcao < 0){
+    if (direcao > 0) {
+        active = active + 1;
+
+        if (active === total) {
+            active = 0;
+        }
+    }
+
+    else if (direcao < 0) {
+        active = active - 1
+
+        if (active < 0) {
+            active = 2
+        }
 
     }
 
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
 
+    numberIndicator.textContent = String(active + 1).padStart(2, '0')
 }
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000);
+
 
 prevButton.addEventListener('click', () => {
     update(-1)
